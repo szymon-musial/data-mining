fc15873f1d15b885162d8e4a7ea02aa61cc61d87
Szymon Musiał <szymongooogle@gmail.com>
2023-03-07 11:29:00 +0100

    Added db context

---
+++ powietrze.gios.gov.pl/Entities/SheetEntity.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace powietrze.gios.gov.pl.Entities;
+
+public class SheetEntity
+{
+    [Key]
+    public int Id { get; set; }
+    public string PollutantName { get; set; }
+
+    public string StationName { get; set; }
+    public double? Value { get; set; }
+
+}
---
+++ powietrze.gios.gov.pl/Persistence/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using powietrze.gios.gov.pl.Entities;
+using System;
+
+namespace powietrze.gios.gov.pl.Persistence;
+
+
+public class AppDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var defaultPgConnectionString = "Host=localhost;Database=DataMining;Username=postgres;Password=root";
+        optionsBuilder.UseNpgsql(defaultPgConnectionString);
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    public DbSet<SheetEntity> SheetEntities { get; set; }
+}
